e7da4379953d2e0650f196a2c2a0765b40197d5b
OChernenko <o.v.bekrenyova@gmail.com>
2018-10-10 15:58:40 +0300

    hw4

--- hw4/js/app.js
+++ hw4/js/app.js
@@ -19,4 +19,84 @@ function multiply() {
 
 multiply();
 
+// 3.
 
+function reverseString(text) {
+   let res = '';
+
+   for (let i = text.length - 1; i >= 0; i--) {
+       res += text[i];
+   }
+   return res;
+}
+
+reverseString('test');
+
+// 4.
+
+function getCodeStringFromText(text) {
+    let res = '';
+    for (let i = 0; i < text.length; i++) {
+        res += text.charCodeAt(i) + ' ';
+    }
+    return res;
+}
+
+getCodeStringFromText('hello');
+
+// Arrays
+
+// 1.
+
+let arr = [1, 2, 3];
+
+function doubleArray(array) {
+    let newArr = array.slice();
+
+    for (let i = 0; i < array.length; i++) {
+        newArr.push(array[i]);
+    }
+
+    return newArr;
+}
+
+doubleArray(arr);
+
+// 2.
+
+function letLastIndexArray(array) {
+    let lastIndex = array[array.length - 1];
+    return lastIndex;
+}
+
+letLastIndexArray(arr);
+
+// 3.
+
+function fillingArray(number) {
+    let newArr = [];
+    let i = 1;
+    while (i < number + 1) {
+        newArr.push(i);
+        i++;
+    }
+
+    return newArr;
+}
+
+fillingArray(100);
+
+// 4.
+
+let arrayFromArrays = [];
+
+function changeCollection() {
+    for (let i = 0; i < arguments.length; i++) {
+        arguments[i].shift();
+        arrayFromArrays.push(arguments[i]);
+    }
+    console.log(arguments);
+    return arrayFromArrays;
+}
+
+changeCollection([2, 3, 4, 8], ['a', 'b', 'c']);
